d343735c81299e8653feba62a753f5ab69ab7878
kidus <104763737+Kidusamt@users.noreply.github.com>
2022-09-08 16:09:24 +0300

    Update free_entire_array.c

--- free_entire_array.c
+++ free_entire_array.c
@@ -1,34 +1,34 @@
 #include "monty.h"
 
 /**
- *  * free_array - frees all elements of
- *   * an array before index j
- *    * @arr: the array
- *     * @j: the index
- *      *
- *       * Return: a void pointer
- *        */
+*  * free_array - frees all elements of
+*   * an array before index j
+*    * @arr: the array
+*     * @j: the index
+*      *
+*       * Return: a void pointer
+*/
 void *free_array(char **arr, int j)
 {
-		int i;
+int i;
 
-			for (i = 0; i < j; i++)
-						free(arr[i]);
-				free(arr);
-					return (NULL);
+for (i = 0; i < j; i++)
+free(arr[i]);
+free(arr);
+return (NULL);
 }
 
 /**
- *  * free_entire_arr - frees the entire array
- *   * @arr: the array
- *    *
- *     * Return: void
- *      */
+*  * free_entire_arr - frees the entire array
+*   * @arr: the array
+*    *
+*     * Return: void
+*/
 void free_entire_arr(char **arr)
 {
-		int i;
+int i;
 
-			for (i = 0; arr[i]; i++)
-						free(arr[i]);
-				free(arr);
+for (i = 0; arr[i]; i++)
+free(arr[i]);
+free(arr);
 }
